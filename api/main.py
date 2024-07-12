e10ae329d7ee7648d1ba9c88bf4c0a55849ece93
TeKayy <th3tk.idk@gmail.com>
2024-07-12 15:20:41 +0200

    Authentication with Linux system local users

--- api/main.py
+++ api/main.py
@@ -1,10 +1,3 @@
-from datetime import datetime, timedelta, timezone
-from typing import Annotated
-
-import json
-import jwt
-import logging
-
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
@@ -12,94 +5,69 @@ from jwt.exceptions import InvalidTokenError
 from passlib.context import CryptContext
 from pydantic import BaseModel
 
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+
 from dotenv import load_dotenv
 from os import getenv
 
+import jwt, pwd, grp, pam, logging
+
 load_dotenv()
 
-USER_DATABASE = './users.db.json'
 SECRET_KEY = getenv('SECRET_KEY')
 ALGORITHM = getenv('ALGORITHM')
 ACCESS_TOKEN_EXPIRE_MINUTES = int(getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
+ACCESS_GROUP_GID = int(getenv('ACCESS_GROUP_GID'))
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], # allow local origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 logging.getLogger('passlib').setLevel(logging.ERROR) # silence the error caused by a bug in the bcrypt package
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 class Token(BaseModel):
     access_token: str
     token_type: str
 
-class TokenData(BaseModel):
-    username: str | None = None
-
 class User(BaseModel):
     username: str
-    full_name: str | None = None
-    permissions: list | None = None
-    disabled: bool | None = None
-
-class UserInDB(User):
-    hashed_password: str
+    uid: int
 
 
-def load_users():
-    with open(USER_DATABASE, 'r') as f:
-        return json.load(f)
+def get_user(username):
+    try:
+        user = pwd.getpwnam(username)
+        return User(username=user.pw_name, uid=user.pw_uid)
+    except KeyError:
+        return None
     
 
-def save_users(users):
-    with open(USER_DATABASE, 'w') as f:
-        json.dump(users, f, indent=4)
-
-        
-def get_user_from_db(username: str):
-    users = load_users()
-    user_data = users.get(username)
-    if user_data:
-        return UserInDB(**user_data)
-    return None
-
-
-def update_user_in_db(username: str, user_data: dict):
-    users = load_users()
-    users[username] = user_data
-    save_users(users)
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-def check_permissions(user, permission):
-    if not user or not user.permissions:
+def is_user_in_access_group(username):
+    try:
+        if username in grp.getgrgid(ACCESS_GROUP_GID): 
+            return True
         return False
-    if ("ADMINISTRATOR" in user.permissions) or (permission in user.permissions): 
-        return True
-    return False
-
+    except:
+        raise Exception('No access group GID set in the configuration')
+    
 
 def authenticate_user(username: str, password: str):
-    user = get_user_from_db(username)
-    if not user:
+    authenticated = pam.authenticate(username, password)  
+    if not authenticated:
         return False
-    if not verify_password(password, user.hashed_password):
+    if not is_user_in_access_group(username):
         return False
-    return user
+    return get_user(username)
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
@@ -122,23 +90,13 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
     except InvalidTokenError:
         raise credentials_exception
-    user = get_user_from_db(username=token_data.username)
+    
+    user = get_user(username)
     if user is None:
         raise credentials_exception
     return user
-
-
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)],
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
 
 
 @app.post("/token")
@@ -157,21 +115,3 @@ async def login_for_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
     return Token(access_token=access_token, token_type="bearer")
-
-
-@app.get("/user", response_model=User)
-async def read_users_me(current_user: Annotated[User, Depends(get_current_active_user)]):
-    return current_user
-
-
-@app.get("/users")
-async def read_users(current_user: Annotated[User, Depends(get_current_active_user)]):
-    if not check_permissions(current_user, "MANAGE_USERS"):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Lacking permissions",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    
-    users = load_users()
-    return [User(**users[key]) for key in users]
