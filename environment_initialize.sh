abb59e0d51b9f92bca92b7334e7f610c3aff7c42
Krzysztoff27 <krzysztof.kolasinski27@outlook.com>
2024-07-15 23:09:01 +0200

    Added code to check whether script is run with sufficient root priviliges

--- environment_initialize.sh
+++ environment_initialize.sh
@@ -4,6 +4,11 @@ error_handler(){
     echo "An error occured! Error code: $?"
 }
 trap "error_handler" ERR
+#Ensure that script is run as root for sufficient priviliges
+if (($EUID != 0)); then
+    echo "Please run the script as root!"
+    exit
+fi
 #repos refresh
 echo -n "[1] Refreshing zypper repositories: "
 zypper -n ref > /dev/null 2>&1
