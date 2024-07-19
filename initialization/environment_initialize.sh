41dd97c54452db0401ab78c913c0aead0dc586d7
Krzysztoff27 <krzysztof.kolasinski27@outlook.com>
2024-07-19 20:08:51 +0200

    Updated initialization script. Added experimental docker-compose file for easier installation of Apache Guacamole containers.

--- initialization/environment_initialize.sh
+++ initialization/environment_initialize.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
-#Force exit on ERR occurence
+#Environmental variables - paths to files storing dependencies names to be installed
+ZYPPER_PACKAGES="./packages.txt"
+
+#Force script to exit on ERR occurence
 set -e
 
-#Error handler to call on ERR occurence
+#Error handler to call on ERR occurence and print an error message
 error_handler(){
     echo "An error occured! Error code: $?"
 }
 trap "error_handler" ERR
 
-#Zypper repos refresh and package installation
-install_packages(){
+#Universal function to read dependenies names from a file
+read_file(){
     packages=()
+
+    while IFS= read -r line || [[ -n "$line" ]]; do
+        packages+=("$line")
+    done < "$1"
+}
+
+#Functions performing installation 
+#Zypper repos refresh and package installation
+install_zypper(){
+    read_file "$ZYPPER_PACKAGES"
     index=1
 
     echo -n "["$index"] Refreshing zypper repositories: "
     zypper -n refresh > /dev/null 2>&1
     echo 'OK'
-
-    while IFS= read -r line || [[ -n "$line" ]]; do
-    packages+=("$line")
-    done < "$1"
 
     for line in "${packages[@]}"; do
         clean_line=$(echo "$line" | tr -cd '[:alnum:][=-=]')
@@ -31,10 +40,11 @@ install_packages(){
     done
 }
 
-#Ensure that script is run as root for sufficient priviliges
+#Test to ensure that script is run with root priviliges
 if (($EUID != 0)); then
-    echo "Please run the script as root!"
+    echo "Insufficient priviliges! Please run the script with root rights."
     exit
 fi
 
-install_packages "./packages.txt"
+#Calls for certain functions - parts of the whole environment initialization process
+install_zypper
