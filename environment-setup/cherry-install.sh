28a9e883c5d2df1f03b3086e3784eea7c2acdbc6
Krzysztoff27 <krzysztof.kolasinski27@outlook.com>
2024-07-30 12:13:53 +0200

    Added support for zypper patterns installation (specificaly for easier kvm installation). Deleted python packages installation from cherry_install.sh script as of Python API being moved into a Docker container.

--- environment-setup/cherry-install.sh
+++ environment-setup/cherry-install.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #Environmental variables - paths to files storing installation logs and dependencies names to be installed
 readonly LOGS_FILE="./logs/installation_logs_"$(date +%d-%m-%y_%H-%M-%S)".txt"
 readonly ZYPPER_PACKAGES="./dependencies/zypper_packages.txt"
-readonly PYTHON_PACKAGES="./dependencies/python_packages.txt" #DIRECTORY TO BE CHANGED IN FINAL VERSION
+readonly ZYPPER_PATTERNS="./dependencies/zypper_patterns.txt"
 
 #Force script to exit on ERR occurence
 set -e
@@ -25,10 +25,10 @@ read_file(){
 
 #Functions performing installation 
 #Zypper repos refresh and package installation
-install_zypper(){
+install_zypper_packages(){
     read_file "$ZYPPER_PACKAGES"
     index=1
-    echo "[Stage I] - Zypper packages installation "
+    echo "[Stage I] - Zypper packages installation"
     echo -n "["$index"] Refreshing zypper repositories: "
     zypper -n refresh >> "$LOGS_FILE" 2>&1 #/dev/null 2>&1
     echo 'OK'
@@ -36,28 +36,25 @@ install_zypper(){
         clean_line=$(echo "$line" | tr -cd '[:alnum:][=-=]')
         ((index++))
         echo -n "["$index"] Installing "$clean_line": "
-        zypper -n install "$clean_line" >> "$LOGS_FILE" 2>&1  #/dev/null 2>&1
+        zypper -n install -t package "$clean_line" >> "$LOGS_FILE" 2>&1  #/dev/null 2>&1
         echo 'OK'
     done
     echo ""
 }
 
-install_python(){
-    read_file "$PYTHON_PACKAGES"
+install_zypper_patterns(){
+    read_file "$ZYPPER_PATTERNS"
     index=1
-    echo "[Stage II] - Python packages installation"
-
-    #INSERT VIRTUAL ENVIRONMENT INITIALIZATION TO PREVENT VERSION CONFLICTS
-
+    echo "[Stage II] - Zypper patterns installation"
     for line in "${packages[@]}"; do
-        clean_line=$(echo "$line" | tr -cd '[:alnum:][[]]')
+        clean_line=$(echo "$line" | tr -cd '[:alnum:][=_=]')
         echo -n "["$index"] Installing "$clean_line": "
         ((index++))
-        #insert pip installation
+        zypper -n install -t pattern "$clean_line" >> "$LOGS_FILE" 2>&1  #/dev/null 2>&1
         echo 'OK'
     done
+    echo ""
 }
-
 #Test to ensure that script is run with root priviliges
 if (($EUID != 0)); then
     echo "Insufficient priviliges! Please run the script with root rights."
@@ -65,5 +62,5 @@ if (($EUID != 0)); then
 fi
 
 #Calls for certain functions - parts of the whole environment initialization process
-install_zypper
-install_python
+install_zypper_packages
+install_zypper_patterns
