2e6c3ffc7e63f64c2c2dc350a4477ee78ff7683e
TeKayy <th3tk.idk@gmail.com>
2024-08-30 14:06:44 +0200

    Removed <Redirect/> component as it has been replaced by react-router-dom native <Navigate/>.

--- react-admin-panel/src/components/Redirect/Redirect.jsx
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom"
-
-export default function Redirect({ to }) {
-    const navigate = useNavigate();
-    useEffect(() => navigate(to), [to])
-    return <></>
-}
