8739b79d6838a7a318d94da975c2d6c244ead718
TeKayy <th3tk.idk@gmail.com>
2024-09-08 18:59:44 +0200

    Multiple utility functions.

--- react-admin-panel/src/utils/misc.js
+++ react-admin-panel/src/utils/misc.js
@@ -7,6 +7,14 @@ export const zipToObject = (keyArray = [], valueArray = []) => noneOrEmpty(keyAr
 
 export const safeObjectValues = (obj = {}) => Object.values({...obj});
 
+export const arrayIntoChunks = (array, chunkSize) => {
+    const result = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+        result.push(array.slice(i, i + chunkSize));
+    }
+    return result;
+};
+
 export const pluralize = (count, singular, pluralFew, pluralMany) => {
     if (count === 1) return singular;
     if ((count % 10 >= 2 && count % 10 <= 4) && !(count % 100 >= 12 && count % 100 <= 14)) {
@@ -15,9 +23,38 @@ export const pluralize = (count, singular, pluralFew, pluralMany) => {
     return pluralMany;
 };
 
+export const getCurrentTime = () => {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+}
+
+/**
+ * setTimeout synchronized with real clock
+ * @param {Function} func function to be called
+ * @param {Number} secondsDelay delay in seconds
+ * @returns clear timeout function
+ */
+export const clockSynchronizedTimeout = (func = () => {}, secondsDelay = 1) => {
+    let timeout;
+    const getDelay = () => 1000 * secondsDelay - new Date().getMilliseconds();;
+    const syncLoadState = () => {
+        func();
+        timeout = setTimeout(syncLoadState, getDelay());
+    };
+    timeout = setTimeout(syncLoadState, getDelay());
+
+    return () => clearTimeout(timeout);
+}
+
 export default {
     isObject,
     noneOrEmpty,
     safeObjectValues,
     pluralize,
+    arrayIntoChunks,
+    getCurrentTime,
+    clockSynchronizedTimeout,
 }
