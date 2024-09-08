4b5ce1e816994694cd4fbab09497444c8c0ee308
TeKayy <th3tk.idk@gmail.com>
2024-09-08 19:01:42 +0200

    PanelLayout's main now supports element stretching.

--- react-admin-panel/src/components/Layouts/PanelLayout.jsx
+++ react-admin-panel/src/components/Layouts/PanelLayout.jsx
@@ -1,12 +1,17 @@
 import { AppShell } from "@mantine/core";
 import { Outlet } from "react-router-dom";
-import NavBar from '../NavBar/NavBar.jsx'
+import NavBar from '../NavBar/NavBar.jsx';
+import styles from './PanelLayout.module.css';
 
-export default function PanelLayout({}) {
+export default function PanelLayout() {
     return (
-        <AppShell navbar={{ width: '75px', breakpoint: 'sm' }} padding="md">
-            <AppShell.Navbar><NavBar/></AppShell.Navbar>
-            <AppShell.Main>
+        <AppShell
+            navbar={{ width: '75px', breakpoint: 'sm' }}
+            padding="md"
+            className={styles.appShell}
+        >
+            <AppShell.Navbar><NavBar /></AppShell.Navbar>
+            <AppShell.Main className={styles.appshellMain}>
                 <Outlet />
             </AppShell.Main>
         </AppShell>
