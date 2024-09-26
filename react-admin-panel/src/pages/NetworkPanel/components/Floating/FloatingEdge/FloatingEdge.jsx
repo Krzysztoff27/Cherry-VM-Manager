32c6f9c456f761111db6e6d67183d80788afc700
TeKayy <th3tk.idk@gmail.com>
2024-09-26 18:18:19 +0200

    Improved connection removal in the Network Panel by introducing the delete button in the middle of the path, for more user-friendly experience.

--- react-admin-panel/src/pages/NetworkPanel/components/Floating/FloatingEdge/FloatingEdge.jsx
+++ react-admin-panel/src/pages/NetworkPanel/components/Floating/FloatingEdge/FloatingEdge.jsx
@@ -1,38 +1,59 @@
-import { getStraightPath, useInternalNode } from '@xyflow/react';
+import { BaseEdge, EdgeLabelRenderer, getStraightPath, useInternalNode, useReactFlow } from '@xyflow/react';
 import { getEdgeParams } from '../utils.js';
-import '../Floating.css';
+import { IconX } from '@tabler/icons-react'
+import classes from '../Floating.module.css';
 
 function FloatingEdge({ id, source, target, markerEnd, style }) {
-  const sourceNode = useInternalNode(source);
-  const targetNode = useInternalNode(target);
+    const { setEdges } = useReactFlow();
+    const sourceNode = useInternalNode(source);
+    const targetNode = useInternalNode(target);
 
-  if (!sourceNode || !targetNode) {
-    return null;
-  }
+    if (!sourceNode || !targetNode) {
+        return null;
+    }
 
-  const { sx, sy, tx, ty, sourcePos, targetPos } = getEdgeParams(
-    sourceNode,
-    targetNode,
-  );
+    const { sx, sy, tx, ty, sourcePos, targetPos } = getEdgeParams(
+        sourceNode,
+        targetNode,
+    );
 
-  const [edgePath] = getStraightPath({
-    sourceX: sx,
-    sourceY: sy,
-    sourcePosition: sourcePos,
-    targetPosition: targetPos,
-    targetX: tx,
-    targetY: ty,
-  });
+    const [edgePath, labelX, labelY] = getStraightPath({
+        sourceX: sx,
+        sourceY: sy,
+        sourcePosition: sourcePos,
+        targetPosition: targetPos,
+        targetX: tx,
+        targetY: ty,
+    });
 
-  return (
-    <path
-      id={id}
-      className="react-flow__edge-path edgePath"
-      d={edgePath}
-      markerEnd={markerEnd}
-      style={style}
-    />
-  );
+    const removeEdge = () => {
+        setEdges((edges) => edges.filter(edge => edge.id !== id))
+    }
+
+    return (
+        <>
+            <BaseEdge
+                id={id}
+                className={classes.edgePath}
+                path={edgePath}
+                markerEnd={markerEnd}
+                style={style}
+            />
+            <EdgeLabelRenderer>
+                <div
+                    style={{ 
+                        position: 'absolute',
+                        transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
+                        pointerEvents: 'all',
+                    }}
+                >
+                    <button className={classes.deleteButton} onClick={removeEdge}>
+                        <IconX size={10} />
+                    </button>
+                </div>
+            </EdgeLabelRenderer>
+        </>
+    );
 }
 
 export default FloatingEdge;
