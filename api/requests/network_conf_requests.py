641e7f9cdc82ecfd627633dc1e69a39236aa2955
TeKayy <th3tk.idk@gmail.com>
2024-10-13 16:16:24 +0200

    Removed redundant viewport property to flow state. Added numbers to dummy intnets.

--- api/requests/network_conf_requests.py
+++ api/requests/network_conf_requests.py
@@ -43,13 +43,9 @@ IntnetConfiguration = dict[IntnetUuid, Intnet]
 class Coordinates(BaseModel):
     x: float = 0
     y: float = 0
-
-class Viewport(Coordinates):
-    zoom: float = 1
     
 class FlowState(BaseModel):
     nodes: list = []
-    viewport: Viewport | None = None
 
 class NetworkConfiguration(FlowState):
     intnets: IntnetConfiguration | None = None
@@ -91,8 +87,8 @@ def get_current_intnet_state() -> IntnetConfiguration: # !
     # ...
     # ? example return:
     return {
-        '5952f2aa-b2c0-4214-a4f7-6ee2c9bf918e': Intnet(uuid='5952f2aa-b2c0-4214-a4f7-6ee2c9bf918e', machines=['b38350cf-105f-4ecd-8eb4-3d9370d39f0e', 'a923601a-fc61-44cb-b007-5df89b1966e2']),  
-        '07e2836c-2854-4347-96e3-6cd9d233af54': Intnet(uuid='07e2836c-2854-4347-96e3-6cd9d233af54', machines=['280af110-b78c-4c7a-a554-d38bc0c428df', '67ac8bfd-2b97-4196-9572-5b519960bf3f']),
+        '5952f2aa-b2c0-4214-a4f7-6ee2c9bf918e': Intnet(number=1, uuid='5952f2aa-b2c0-4214-a4f7-6ee2c9bf918e', machines=['b38350cf-105f-4ecd-8eb4-3d9370d39f0e', 'a923601a-fc61-44cb-b007-5df89b1966e2']),  
+        '07e2836c-2854-4347-96e3-6cd9d233af54': Intnet(number=2, uuid='07e2836c-2854-4347-96e3-6cd9d233af54', machines=['280af110-b78c-4c7a-a554-d38bc0c428df', '67ac8bfd-2b97-4196-9572-5b519960bf3f']),
     }
 
 def isIndexInList(_list, index):
